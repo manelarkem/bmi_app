01359ea922244269a65093cbdada95603ef5d805
manel <manelarkem@gmail.com>
2022-07-13 15:53:38 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        canvasColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.white),
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 45,
+            fontWeight: FontWeight.w800,
+            color: Colors.black,
+          ),
+          headline2: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          bodyText1: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
